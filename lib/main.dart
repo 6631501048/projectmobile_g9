836747d12fb8f0ddc17f7d2bbb99516eee972d15
6631501034 <6631501034@lamduan.mfu.ac.th>
2025-10-25 13:47:59 +0700

    staff_history

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:projectmobile_g9/Lect/lect_history.dart';
 import 'package:projectmobile_g9/Staff/staff_history.dart';
-import 'package:projectmobile_g9/Student/student_history.dart';
-
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, 
-  home: StudentHistory()
+   runApp(MaterialApp(debugShowCheckedModeBanner: false, 
+  home: StaffHistory()
   ));
-  //  runApp(MaterialApp(debugShowCheckedModeBanner: false, 
-  // home: StaffHistory()
-  // ));
-  //   runApp(MaterialApp(debugShowCheckedModeBanner: false, 
-  // home: Lecthistory()
-  // ));
+
 }
