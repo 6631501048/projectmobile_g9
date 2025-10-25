cfa0a655231d696ce3d541b751bc9369f41fcc63
6631501034 <6631501034@lamduan.mfu.ac.th>
2025-10-25 13:44:48 +0700

    lect_history

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:projectmobile_g9/Lect/lect_history.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, 
-  home:
+  home:Lecthistory()
   ));
 }
